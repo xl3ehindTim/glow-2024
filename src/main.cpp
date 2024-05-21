250bfe1906fdffe166806a85639b4c75ee4e8212
BramVanKempen <140656244+BramVanKempen@users.noreply.github.com>
2024-05-21 13:38:44 +0200

    feat/added-mp3

--- src/main.cpp
+++ src/main.cpp
@@ -14,23 +14,24 @@ SoftwareSerial mp3(6, 7);
 
 static int8_t Send_buf[8] = {0}; //-> Buffer to send commands.
 
-/*
-DMX uses multiple channels for rgb and brightness
-*/
+int currentVolume = 0;
+
+// DMX uses multiple channels for RGB and brightness
 const int maxChannels = 4;
 
-// Initialize rgb variables
-int redValue = 0;
+// Initialize RGB variables
+int brightness = 0;
 
+// Pin connected to touch sensor
 const int buttonPin = 11;
 
-boolean isTouched()
-{
+// Function to check if the button is touched
+boolean isTouched() {
   return digitalRead(buttonPin) == HIGH;
 }
 
-void sendCommand(byte command, byte dat1, byte dat2)
-{
+// Function to send command to MP3 module
+void sendCommand(byte command, byte dat1, byte dat2) {
   delay(20);
   Send_buf[0] = 0x7E;    //-> Every command should start with $(0x7E)
   Send_buf[1] = 0xFF;    //-> Version information
@@ -41,27 +42,27 @@ void sendCommand(byte command, byte dat1, byte dat2)
   Send_buf[6] = dat2;    //-> data2
   Send_buf[7] = 0xEF;    //-> Ending byte of the command
 
-  for (uint8_t i = 0; i < 8; i++)
-  {
+  for (uint8_t i = 0; i < 8; i++) {
     mp3.write(Send_buf[i]);
   }
 }
 
-void setupDmx()
-{
-  /*
-  Setup DMX, uses multiple channels for brightness, red, green, blue and any other usage
-  depending on the device.
-  */
+// Function to set up DMX
+void setupDmx() {
+  // Setup DMX, uses multiple channels for brightness, red, green, blue and any other usage
+  // depending on the device.
   DmxMaster.usePin(3);
-  DmxMaster.maxChannel(4);
+  DmxMaster.maxChannel(maxChannels);
 
   // Set brightness to full
   DmxMaster.write(1, 255);
+  DmxMaster.write(2, 255);
+  DmxMaster.write(3, 0);
+  DmxMaster.write(4, 0);
 }
 
-void setupMp3(int port = 9600)
-{
+// Function to set up MP3 player
+void setupMp3(int port = 9600) {
   mp3.begin(port);
 
   sendCommand(CMD_SEL_DEV, 0, DEV_TF);
@@ -70,33 +71,64 @@ void setupMp3(int port = 9600)
   sendCommand(CMD_PLAY, 0, 0);
 }
 
-void setup()
-{
+// Function to adjust MP3 volume
+void volumeMP3(int newVolume) {
+  if (currentVolume == newVolume) {
+    Serial.println("Volume is the same");
+    return;
+  } else if (newVolume == 0) {
+    newVolume = 1;
+  }
+  while (currentVolume < newVolume)
+  {
+    sendCommand(CMD_VOLUME_UP, 0, 0);
+    currentVolume++;
+  }
+  while (currentVolume > newVolume)
+  {
+    if (currentVolume == 1) {
+      break;
+    }
+    sendCommand(CMD_VOLUME_DOWN, 0, 0);
+    currentVolume--;
+  }
+  Serial.print("Volume is now: ");
+  Serial.println(currentVolume);
+}
+
+void setup() {
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
 
   setupMp3();
   setupDmx();
+  volumeMP3(1);
+
 }
 
-void loop()
-{
-  if (isTouched())
-  {
-    // Ensures value doesn't exceed 255
-    redValue = min(redValue + 1, 255); 
-    Serial.println(redValue);
-    DmxMaster.write(2, redValue);
-    sendCommand(CMD_VOLUME_UP, 0, 0);
+void loop() {
+  if (isTouched()) {
+    // Ensure value doesn't exceed 255
+    brightness = min(brightness + 1, 255);
+    DmxMaster.write(1, brightness);
+    //int callculateVolume = brightness / 255;
+    int newVolume = brightness / 8.5;
+    Serial.print(brightness);
+    Serial.print(" - ");
+    Serial.println(newVolume);
+    volumeMP3(newVolume);
   }
 
   // Decrease red value gradually
-  if (redValue != 0 && !isTouched())
-  {
-    redValue = redValue - 1;
-    DmxMaster.write(2, redValue);
-
-    sendCommand(CMD_VOLUME_DOWN, 0, 0);
+  if (brightness != 0 && !isTouched()) {
+    brightness = brightness - 1;
+    DmxMaster.write(1, brightness);
+    //.int callculateVolume = brightness / 255;
+    int newVolume = brightness / 8.5;
+    Serial.print(brightness);
+    Serial.print(" - ");
+    Serial.println(newVolume);
+    volumeMP3(newVolume);
   }
 
   delay(25);
