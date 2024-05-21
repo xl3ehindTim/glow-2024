28ac8e7683d86ef80b0b4781beac00043eb99248
Tim <timbrouwers1@gmail.com>
2024-05-21 10:56:17 +0200

    Added mp3 code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include <Arduino.h>
 #include <DmxMaster.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial mp3(6, 7);
+
+#define CMD_VOLUME_UP 0X04   //-> Volume increased one.
+#define CMD_VOLUME_DOWN 0X05 //-> Volume decrease one.
+#define CMD_SET_VOLUME 0X06  //-> Set volume.
+#define CMD_PLAY 0X0D        //-> Resume playback
+
+#define CMD_SEL_DEV 0X09 //-> Select device.
+#define DEV_TF 0X02      //-> 02 for TF card.
+
+static int8_t Send_buf[8] = {0}; //-> Buffer to send commands.
 
 /*
 DMX uses multiple channels for rgb and brightness
@@ -11,43 +24,80 @@ int redValue = 0;
 
 const int buttonPin = 11;
 
+boolean isTouched()
+{
+  return digitalRead(buttonPin) == HIGH;
+}
+
+void sendCommand(byte command, byte dat1, byte dat2)
+{
+  delay(20);
+  Send_buf[0] = 0x7E;    //-> Every command should start with $(0x7E)
+  Send_buf[1] = 0xFF;    //-> Version information
+  Send_buf[2] = 0x06;    //-> The number of bytes of the command without starting byte and ending byte
+  Send_buf[3] = command; //-> Such as PLAY and PAUSE and so on
+  Send_buf[4] = 0x01;    //-> 0x00 = not feedback, 0x01 = feedback
+  Send_buf[5] = dat1;    //-> data1
+  Send_buf[6] = dat2;    //-> data2
+  Send_buf[7] = 0xEF;    //-> Ending byte of the command
+
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    mp3.write(Send_buf[i]);
+  }
+}
+
+void setupDmx()
+{
+  /*
+  Setup DMX, uses multiple channels for brightness, red, green, blue and any other usage
+  depending on the device.
+  */
+  DmxMaster.usePin(3);
+  DmxMaster.maxChannel(4);
+
+  // Set brightness to full
+  DmxMaster.write(1, 255);
+}
+
+void setupMp3(int port = 9600)
+{
+  mp3.begin(port);
+
+  sendCommand(CMD_SEL_DEV, 0, DEV_TF);
+  delay(500);
+  
+  sendCommand(CMD_PLAY, 0, 0);
+}
+
 void setup()
 {
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
 
-  // Select pin
-  DmxMaster.usePin(3);
-
-  // Set max channel (brightness, red, green, blue)
-  DmxMaster.maxChannel(4);
-
-  DmxMaster.write(1, 255);      
-}
-
-boolean isTouched()
-{
-  return digitalRead(buttonPin) == HIGH;
+  setupMp3();
+  setupDmx();
 }
 
 void loop()
 {
   if (isTouched())
   {
-    // Increase red value gradually (e.g., by 10)
-    redValue = min(redValue + 1, 255); // Ensures value doesn't exceed 255
+    // Ensures value doesn't exceed 255
+    redValue = min(redValue + 1, 255); 
     Serial.println(redValue);
-    DmxMaster.write(2, redValue); 
+    DmxMaster.write(2, redValue);
+    sendCommand(CMD_VOLUME_UP, 0, 0);
   }
-  else
+
+  // Decrease red value gradually
+  if (redValue != 0 && !isTouched())
   {
-    while (redValue != 0 && !isTouched())
-    {
-      // Decrease red value gradually
-      redValue = redValue - 1;      
-      DmxMaster.write(2, redValue);
-      delay(25);
-    }
+    redValue = redValue - 1;
+    DmxMaster.write(2, redValue);
+
+    sendCommand(CMD_VOLUME_DOWN, 0, 0);
   }
+
   delay(25);
 }
